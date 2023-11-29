0bd961a75b2d607a641c4ed138d37c9e6652b130
sajid360 <ahteshamsajid8@gmail.com>
2023-11-30 00:51:49 +0600

    payment history related api added

--- index.js
+++ index.js
@@ -193,6 +193,11 @@ async function run() {
       });
     });
 
+    app.get('/payments/:email', async (req, res) => {
+      const query = { email: req.params.email }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result)
+    })
     app.post('/payments', async (req, res) => {
       const payment = req.body;
       const paymentResult = await paymentCollection.insertOne(payment);
